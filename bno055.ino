f371676319a2a154eabfb6253f25106c236197cd
ShriramUmashankar <132140196+ShriramUmashankar@users.noreply.github.com>
2023-10-25 00:10:08 +0530

    Add files via upload
    Code for bno (to be extended to get other parameter detials)

---
+++ bno055.ino
@@ -0,0 +1,39 @@
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BNO055.h>
+#include <utility/imumaths.h>
+  
+Adafruit_BNO055 bno = Adafruit_BNO055(55);
+
+void setup(void) 
+{
+  Serial.begin(115200);
+  Serial.println("Orientation Sensor Test"); Serial.println("");
+  
+  /* Initialise the sensor */
+  if(!bno.begin())
+  {
+    /* There was a problem detecting the BNO055 ... check your connections */
+    Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
+    while(1);
+  }
+  
+  delay(1000);
+    
+  bno.setExtCrystalUse(true);
+}
+
+void loop(void) 
+{
+  imu::Vector<3> angular_vel = bno.getVector(Adafruit_BNO055::VECTOR_GYROSCOPE);
+
+  Serial.print(" X: ");
+  Serial.print(angural_vel.x(), 4);
+  Serial.print(" Y: ");
+  Serial.print(angural_vel.y(), 4);
+  Serial.print(" Z: ");
+  Serial.print(angural_vel.z(), 4);
+  Serial.println("");
+  
+  delay(100);
+}
